2c653e44f329cdb3661a3a785d00930d36401ee1
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-24 19:44:08 +0530

    Render header component to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
+    <>
+    <Header />
     <div className="App">
       <h2>Hello</h2>
     </div>
+    </>
   );
 }
 

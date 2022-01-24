d5c32d63f244143fc82c6367b711f9bae2a2653c
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-24 19:40:48 +0530

    Create component Header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = ()=>{
+    return (
+        <header>
+            <img src="../asset/logo.png"></img>
+            <nav>
+
+            </nav>
+        </header>
+    );
+};
+
+export default Header;

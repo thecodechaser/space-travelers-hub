aeecc93bf13349982bb5b9e42d6d2ed1370b8fd7
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-25 16:18:35 +0530

    Add action for fetch rockets

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,13 +1,24 @@
 import Axios from 'axios';
 
+// conts
+const FETCH_ROCKET = 'spaceTravelersHub/rockets/FETCH_ROCKET';
+
 // actions
+const fetchRocket= (payload) => ({
+    type: FETCH_ROCKET,
+    payload,
+  });
+
+//   apis-functions
 
 
 // state
-
 const initialState = [];
 
+// reducer
 const reducer = (state = initialState, action) => {
+   
+    }
 };
 
 export default reducer;

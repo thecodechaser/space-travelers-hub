954cdb2ce8811229245e0d65ce1b03ec327b81ef
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-25 18:20:47 +0530

    Apply styling

--- src/components/rocketsComponent/RocketItem.js
+++ src/components/rocketsComponent/RocketItem.js
@@ -5,10 +5,10 @@ const RocketItem = (props) => {
   const { rocket: { name, image, description } } = props;
   return (
     <div className="rocket-container">
-      <img alt="rocket" src={image} />
+      <img alt="rocket" src={image} className="rocket-img" />
       <div className="rocket-details">
-        <h2>{name}</h2>
-        <p>{description}</p>
+        <h2 className="rocket-title">{name}</h2>
+        <p className="rocket-details">{description}</p>
       </div>
     </div>
   );

c0edf3f1389ee347ad4b86653365fd0fe568852a
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-25 11:49:56 +0530

    Add MyProfile component

---
+++ src/components/MyProfile.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const MyProfile =()=>{
+    return (
+        <main className="profile-main">
+            <div className="profile-rockets">
+                <h1>My Rockets</h1>
+            </div>
+            <div className="profile-missions">
+                <h1>My Missions</h1>
+            </div>
+        </main>
+    );
+}
+
+export default MyProfile;

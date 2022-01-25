be29a049e4fda3989a3d94b19ced8a9c3b622101
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-25 15:02:33 +0530

    Apply alignment

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -2,35 +2,34 @@ import React from 'react';
 import RocketItem from './rocketsComponent/RocketItem';
 
 const Rockets = () => {
-
   const rockets = [
     {
       id: 0,
-      name: "name 1",
-      image: "https://imgur.com/DaCfMsj.jpg",
-      description: "details 1"
+      name: 'name 1',
+      image: 'https://imgur.com/DaCfMsj.jpg',
+      description: 'details 1',
     },
     {
       id: 1,
-      name: "name 2",
-      image: "https://imgur.com/DaCfMsj.jpg",
-      description: "details 2"
+      name: 'name 2',
+      image: 'https://imgur.com/DaCfMsj.jpg',
+      description: 'details 2',
     },
     {
       id: 2,
-      name: "name 3",
-      image: "https://imgur.com/DaCfMsj.jpg",
-      description: "details 3"
-    }
-  ]
+      name: 'name 3',
+      image: 'https://imgur.com/DaCfMsj.jpg',
+      description: 'details 3',
+    },
+  ];
   return (
-  <main>
-    {
-      rockets.map(rocket=>(
-        <RocketItem rocket={rocket}/>
+    <main>
+      {
+      rockets.map((rocket) => (
+        <RocketItem key={rocket.id} rocket={rocket} />
       ))
     }
-  </main>
+    </main>
   );
 };
 

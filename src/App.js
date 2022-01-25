f43f09d66cff35745f5f638e3fdb014bc9880793
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-25 11:52:55 +0530

    Import myprofile to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import './styles/MyProfile.css';
 import './styles/App.css';
 import './styles/Header.css';
 import Header from './components/Header';
+import MyProfile from './components/MyProfile';
 
 function App() {
   return (
     <>
       <Header />
-      <div className="App" />
+      <MyProfile />
     </>
   );
 }

fcbcd29bf1c9b2c91e0bf67fa976a08a14be0bb2
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-25 16:46:11 +0530

    Fix liner erros

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -4,38 +4,40 @@ import Axios from 'axios';
 const FETCH_ROCKET = 'spaceTravelersHub/rockets/FETCH_ROCKET';
 
 // actions
-const fetchRocket= (payload) => ({
-    type: FETCH_ROCKET,
-    payload,
-  });
+const fetchRocket = (payload) => ({
+  type: FETCH_ROCKET,
+  payload,
+});
 
 //   apis-functions
 
-export const fetchRocketApi= () => async (dispatch) =>{
-    const data = await Axios.get('https://api.spacexdata.com/v3/rockets');
-    const rockets = [];
-    for(let i=0; i<data.length; i++){
-        const name = data[i].rocket_name;
-        const id = data[i].id;
-        const description = data[i].description;
-        const image = data[i].flickr_images[0];
-        const object = { id, name, description, image };
-        rockets.push(object);
-    }
-    dispatch(fetchRocket(rockets));
-}
+export const fetchRocketApi = () => async (dispatch) => {
+  const data = await Axios.get('https://api.spacexdata.com/v3/rockets');
+  const rockets = [];
+  for (let i = 0; i < data.length; i += 1) {
+    const name = data[i].rocket_name;
+    const { id } = data[i];
+    const { description } = data[i];
+    const image = data[i].flickr_images[0];
+    const object = {
+      id, name, description, image,
+    };
+    rockets.push(object);
+  }
+  dispatch(fetchRocket(rockets));
+};
 
 // state
 const initialState = [];
 
 // reducer
 const reducer = (state = initialState, action) => {
-    switch(action.type){
-        case FETCH_ROCKET:
-            return action.payload;
-        default:
-          return state;
-    }
+  switch (action.type) {
+    case FETCH_ROCKET:
+      return action.payload;
+    default:
+      return state;
+  }
 };
 
 export default reducer;

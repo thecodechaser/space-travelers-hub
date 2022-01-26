484875272e8eb7e9d93010c1e35f336865f6afa9
KayLemba <kaylemba@yahoo.com>
2022-01-26 16:48:17 +0200

    Add missions profile

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import MissionProfile from './missionspage/missionsProfile';
 
 const MyProfile = () => {
   const rockets = useSelector((state) => state.rocketsReducer);
   const reservedRockets = rockets.filter((rocket) => rocket.reserved);
-
   return (
     <main className="profile-main">
       <div>
         <h1>My Rockets</h1>
         <div className="profile-rockets">
-          {
-          reservedRockets.map((rocket) => (
+          {reservedRockets.map((rocket) => (
             <h3 key={rocket.id} className="profile-res-rocket">{rocket.name}</h3>
-          ))
-        }
+          ))}
         </div>
       </div>
-      <div>
-        <h1>My Missions</h1>
+      <div className="profile-rockets">
+        <MissionProfile />
       </div>
     </main>
   );

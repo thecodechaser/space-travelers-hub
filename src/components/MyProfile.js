74fed57426e7d3de0848f5424a243171407303bf
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-26 18:14:52 +0530

    Fix linter erros

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -2,25 +2,25 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 const MyProfile = () => {
-const rockets = useSelector((state) => state.rocketsReducer);
-const reservedRockets = rockets.filter((rocket) => rocket.reserved);
+  const rockets = useSelector((state) => state.rocketsReducer);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved);
 
   return (
-  <main className="profile-main">
-    <div>
-      <h1>My Rockets</h1>
-      <div className="profile-rockets">
-        {
-          reservedRockets.map((rocket)=>(
-            <h3 className="profile-res-rocket">{rocket.name}</h3>
+    <main className="profile-main">
+      <div>
+        <h1>My Rockets</h1>
+        <div className="profile-rockets">
+          {
+          reservedRockets.map((rocket) => (
+            <h3 key={rocket.id} className="profile-res-rocket">{rocket.name}</h3>
           ))
         }
-      </div>
+        </div>
       </div>
       <div>
-      <h1>My Missions</h1>
+        <h1>My Missions</h1>
       </div>
-  </main>
+    </main>
   );
 };
 

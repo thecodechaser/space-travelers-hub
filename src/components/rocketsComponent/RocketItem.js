e0876609cff793e82ee3b83f9f27cd20f7b53506
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-26 12:13:46 +0530

    Dispatch action reserve rocket

--- src/components/rocketsComponent/RocketItem.js
+++ src/components/rocketsComponent/RocketItem.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { reserveRocket } from '../../redux/rockets/rockets';
 
 const RocketItem = (props) => {
-  const { rocket: { id, name, image, description } } = props;
+  const { rocket: { id, name, image, description, reserved } } = props;
+  console.log(reserved);
+  const dispatch = useDispatch();
+  const reserveRocketH=()=>{
+    dispatch(reserveRocket(id));
+  }
   return (
     <div className="rocket-container">
       <img alt="rocket" src={image} className="rocket-img" />
       <div className="rocket-details">
         <h2 className="rocket-title">{name}</h2>
         <p className="rocket-details">{description}</p>
-        <button type="button" className="rocket-reserve-btn">Reserve Rocket</button>
+        <button type="button" className="rocket-reserve-btn" onClick={reserveRocketH}>Reserve Rocket</button>
       </div>
     </div>
   );

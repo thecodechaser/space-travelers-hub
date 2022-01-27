9505fe6d624923f633ac85a447a1969d80f37ba1
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-27 14:57:44 +0530

    Finilize styling

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import MissionProfile from './missionspage/missionsProfile';
+import RocketProfile from './rocketsComponent/RocketProfile';
 
-const MyProfile = () => {
-  const rockets = useSelector((state) => state.rocketsReducer);
-  const reservedRockets = rockets.filter((rocket) => rocket.reserved);
-  return (
-    <main className="profile-main">
-      <div>
-        <h1>My Rockets</h1>
-        <div className="profile-rockets">
-          {reservedRockets.map((rocket) => (
-            <h3 key={rocket.id} className="profile-res-rocket">{rocket.name}</h3>
-          ))}
-        </div>
-      </div>
-      <div className="profile-rockets">
-        <MissionProfile />
-      </div>
-    </main>
-  );
-};
+const MyProfile = () => (
+
+  <main className="profile-main">
+    <MissionProfile />
+    <RocketProfile />
+  </main>
+);
 
 export default MyProfile;

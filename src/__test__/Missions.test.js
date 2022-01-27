11227256f014fe9b809434808c2f50104f4fa691
KayLemba <kaylemba@yahoo.com>
2022-01-27 16:52:30 +0200

    Add tests for missions

---
+++ src/__test__/Missions.test.js
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
+import { Provider } from 'react-redux';
+import Missions from '../components/missionsPage/Missions';
+import store from '../redux/configureStore';
+
+describe('Missions tests', () => {
+  test('should render', () => {
+    const missions = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Missions />
+        </BrowserRouter>
+        ,
+      </Provider>,
+    );
+
+    expect(missions).toMatchSnapshot();
+  });
+});
--- src/__test__/Rockets.test.js
+++ src/__test__/Rockets.test.js
@@ -1,18 +1,19 @@
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import '@testing-library/jest-dom';
-import Rockets from '../components/Rockets'
 import { Provider } from 'react-redux';
+import Rockets from '../components/Rockets';
 import store from '../redux/configureStore';
 
 describe('Rockets tests', () => {
   test('should render', () => {
     const rockets = render(
-        <Provider store={store}>
-      <BrowserRouter>
-        <Rockets />
-      </BrowserRouter>,
-      </Provider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Rockets />
+        </BrowserRouter>
+        ,
+      </Provider>,
     );
 
     expect(rockets).toMatchSnapshot();
